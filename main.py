94b5d12ce99996e956b3ed7c802cd4f1c6cc7d51
andrey-belikov <abelikov2602@gmail.com>
2024-12-21 23:50:35 +0300

    Добавлено ограничение фпс - 60

--- main.py
+++ main.py
@@ -11,12 +11,13 @@ def main():
     screen = pygame.display.set_mode((width, height))
     pygame.display.set_caption("Турель против астероидов")
     game_scene = MainScene(screen)
+    clock = pygame.time.Clock()
     run = True
     while run:
         run = game_scene.run_game()
+        clock.tick(60)  # limits FPS to 60
     pygame.quit()
 
 
 if __name__ == '__main__':
     main()
-
